961ddebe8c3a9763abd9081375da9efb7129edac
edinsonRmz <edinsonramirez0627@gmail.com>
2023-09-13 18:49:11 -0500

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 
 import Home from './Components/Home/Home';
-import Text from './Components/Text/text';
+import Text from './Components/Text/Text';
 import Proyectos from './Components/Proyectos/Proyectos';
 import Tecno from './Components/Tecnologias/Tecnologias';
-import Wrapper from './Components/Wrapper/Wrapper'
-import Copy from './Components/copyright/copyright'
-import Styls from './App.css'
+import Wrapper from './Components/Wrapper/Wrapper';
+import Copyright from './Components/copyright/copyright';
 
 function diie() {
   return (
     <div className="App">
-      <Home/>
-      <Text/>
-      <Proyectos/>
-      <Tecno/>
-      <Wrapper/>
-      <Copy/>
-
-  </div>
-  )
+      <Home />
+      <Text />
+      <Proyectos />
+      <Tecno />
+      <Wrapper />
+      <Copyright />
+    </div>
+  );
 }
 
-export default diie 
+export default diie;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <BrowserRouter basename="/<nombre-del-repositorio>">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
