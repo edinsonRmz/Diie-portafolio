0fc09b9253126ef112e917f2c9b835404a399a68
edinsonRmz <edinsonramirez0627@gmail.com>
2023-09-13 14:41:56 -0500

    1.0.1

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: 'https://github.com/edinsonRmz/Diie-portafolio',
-  server: {
-    host: true,
-    open: true
-  }
+ 
 })
 
